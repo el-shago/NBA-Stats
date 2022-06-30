19f5dc37cdd35d5b480f17be20fd5c583d0bb36c
el-shago <92878520+el-shago@users.noreply.github.com>
2022-06-30 12:40:34 +0200

    Nombres de los equipos

--- nba-scores.py
+++ nba-scores.py
@@ -6,5 +6,28 @@ ALL_JSON = "/prod/v1/today.json"
 
 printer =  PrettyPrinter()
 
-data = get(BASE_URL + ALL_JSON).json()
-printer.pprint(data)
+
+def get_links():
+    data = get(BASE_URL + ALL_JSON).json()
+    links = data['links']
+    return links
+
+
+def get_scoreboard():
+    scoreboard = get_links()['currentScoreboard']
+    games = get(BASE_URL + scoreboard).json()['games']
+
+    for game in games:
+        home_team = game['hTeam']
+        away_team = game['vTeam']
+        clock = game['clock']
+        period = game['period']
+
+        print("-------------------------------------------------")
+        print(f"{home_team['triCode']} vs {away_team['triCode']}, {clock}, {period} ")
+
+
+    
+
+
+get_scoreboard()
