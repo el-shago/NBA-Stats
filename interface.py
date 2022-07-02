34e707a5b63e9ae32ac8c80f55d216ddbdba626c
el-shago <92878520+el-shago@users.noreply.github.com>
2022-07-02 19:47:58 +0200

    Primeros avances chidos

---
+++ interface.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from turtle import position
+from weakref import WeakSet
+import nba_now
+from nba_now import *
+
+main_window = Tk()
+main_window.geometry("420x420")
+main_window.title("NBA NOW")
+
+#Label
+Title = Label(main_window, text= "NBA NOW",
+font=('Arial', 25, 'bold'),
+relief = RAISED,
+bd=2,
+padx=20,
+pady=20,
+justify=CENTER)
+Title.place(relx= 0.5, rely=0.030, anchor=N)
+
+#Buttons
+score = Button(main_window, text= "Scoreboards",
+font=('Arial', 12, 'bold'),
+justify=CENTER,
+command=get_scoreboard)
+score.place(relx=0.2, rely=0.5, anchor=W)
+
+leaders = Button(main_window, text= "Team Leaders", command=get_stats)
+leaders.place(relx=0.8, rely=0.5, anchor=E)
+players = Button(main_window, text= "Player Info", command=get_players)
+players.place(relx=0.5, rely=0.8, anchor=S)
+
+main_window.mainloop()
